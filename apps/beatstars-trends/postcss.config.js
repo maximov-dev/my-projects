4230281a590993a950c89246d70c8c91f67ecd2b
Yaroslav Maximov <starspeen@gmail.com>
2023-01-05 16:32:53 +0300

    feat(beatstars-trends/deploy): set up tailwind

--- apps/beatstars-trends/postcss.config.js
+++ apps/beatstars-trends/postcss.config.js
@@ -1,9 +1,9 @@
-const path = require('path');
+const { join } = require('path');
 
 module.exports = {
   plugins: {
     tailwindcss: {
-      config: path.join(__dirname, 'tailwind.config.js'),
+      config: join(__dirname, 'tailwind.config.js'),
     },
     autoprefixer: {},
   },
--- apps/beatstars-trends/tailwind.config.js
+++ apps/beatstars-trends/tailwind.config.js
@@ -1,17 +1,9 @@
-const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
 const { join } = require('path');
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    join(
-      __dirname,
-      '{app, src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}'
-    ),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
+  content: [join(__dirname, '/src/**/*.{ts,tsx,html}')],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
